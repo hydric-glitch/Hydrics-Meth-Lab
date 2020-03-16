5d361134fe77f8df6a1141c63506fe1f2b358e4e
hydric-glitch <62049462+hydric-glitch@users.noreply.github.com>
2020-03-16 00:56:53 -0500

    Create jobs.lua

---
+++ jobs.lua
@@ -0,0 +1,14 @@
+TEAM_METHCOOK = DarkRP.createJob("Meth Cook", {
+    color = Color(0, 147, 166, 255),
+    model = {"models/Kleiner.mdl"},
+    description = [[The Meth Cook, cooks high quality methamphetamine to sell!]],
+    weapons = {},
+    command = "methcook",
+    max = 4,
+    salary = 50,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = true,
+    category = "Gangsters"
+})
